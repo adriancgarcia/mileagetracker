cc9bef771edb29d3d48924ecaae9a7387ee42c57
adriancgarcia <adriancgarcia@gmail.com>
2023-10-14 10:02:24 -0500

    created getTrips function

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { onMount} from "svelte"
+  import Display from './components/Display.svelte'
+  import Form from './components/Form.svelte'
+
+  const url="https://mileagetrackerbackend-ag.onrender.com/trips";
+
+  // state 
+  let showForm = false
+  // let reminder = ""
+  let completed = false
+  let _id = ""
+  let trips = []
+  let action = "create"
+
+  // functions
+  const getTrips = async () => {
+    const response = await fetch (url)
+    const data = await response.json()
+    trips = data
+  }
+
+// Lifecycle
+onMount (() => {getTrips()})
+</script>
+
+<main>
+  <h1>
+    <img src="/img/vecteezy_vector-speedometer-icon_.jpg" alt="odometer">
+</h1>
+  <h1>Trips Made</h1>
+  <Display />
+  {#if showForm}
+  <Form />
+  {/if}
+</main>
+
+<style>
+  img {
+        max-width: 300px;
+    }
+</style>
